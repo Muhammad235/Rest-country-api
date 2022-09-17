c7ed8bc107b34860df9917f4edba7fdbcab981c6
Muhammad235 <adelekeyahaya05@gmail.com>
2022-09-17 13:35:35 +0100

    fetched the api and got the data

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const searchInput = document.getElementById('country-name')
 const searchBtn = document.getElementById('search-btn')
 
 
+
 // fetching country data from https://restcountries.com/v3.1/name/{name}
 
 
@@ -15,7 +16,27 @@ const getCountry = () =>{
 
 fetch(`https://restcountries.com/v3.1/name/${inputValue}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+
+        console.log(data[0].capital);
+        console.log(data[0].flags);
+        console.log(data[0].population);
+        console.log(data[1].continents);
+    })
+
+
+
+    // let countryDetails = `
+    //     <figcaption>
+    //         <h3 id="country-name">Germany</h3>
+    //         <p>Population: <span id="population">81,770,900</span> </p>
+    //         <p>Region: <span>Europe</span> </p>
+    //         <p>Capital: <span>Berlin</span></p>
+    //     </figcaption>
+    // `
+
+
 }
 
 
